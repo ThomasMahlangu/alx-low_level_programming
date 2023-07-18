349713205be0f7493c5065a3fd7127958b154577
ThandoMahlangu <thandomahlangu09@gmail.com>
2023-07-18 06:45:32 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,18 +1,16 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * _isalpha - checks for alphatic character
- * @c: The character to be checked
- * Return: 1 for alphabetic character or 0 for anything else
- */
-
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
 int _isalpha(int c)
-
 {
-
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
-	return (1);
+		return (1);
 	}
+
 	return (0);
-}
+}}
